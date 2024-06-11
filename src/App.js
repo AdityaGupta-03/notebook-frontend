fd130f9056b6b41a12ff3c4a543f071ec2661c26
Aditya Gupta <iaditya.gupta03@gmail.com>
2024-06-12 00:50:45 +0530

    Adding Form in Home Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import Navbar from './components/Navbar.js';
-import Home from './components/Home.js';
-import About from './components/About.js';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import NotesState from "./context/Notes/NotesState.js";
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div className='container'>
-      This is Home
-    </div>
+    <>
+      <div className='container my-3'>
+        <h2>Add your Note</h2>
+        <form>
+          <div class="mb-3">
+            <label for="title" class="form-label">Title</label>
+            <input type="text" class="form-control" id="title" aria-describedby="emailHelp" name='title'/>
+          </div>
+          <div class="mb-3">
+            <label for="desc" class="form-label">Description</label>
+            <textarea class="form-control" id="desc" rows="3" name='desc'></textarea>
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+      </div>
+      <div className="container my-3">
+        <h2>Your Notes</h2>
+      </div>
+    </>
+
   )
 }
