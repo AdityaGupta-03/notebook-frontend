cd0eba3bcd5b348884d2574f3458662c8421e9af
Aditya Gupta <iaditya.gupta03@gmail.com>
2024-06-11 17:20:13 +0530

    Implementing Navbar and Route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React from 'react'
+import Navbar from './components/Navbar.js';
+import Home from './components/Home.js';
+import About from './components/About.js';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
