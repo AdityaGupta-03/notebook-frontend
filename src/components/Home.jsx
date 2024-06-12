68d1a24fc7c8ec8b1bf73d6d0bfa0b76d4297844
Aditya Gupta <iaditya.gupta03@gmail.com>
2024-06-12 20:13:40 +0530

    Fetching Hardcoded notes from context

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import notesContext from '../context/Notes/NotesContext'
 
 export default function Home() {
+  const context = useContext(notesContext);
+
   return (
     <>
       <div className='container my-3'>
@@ -11,7 +14,7 @@ export default function Home() {
             <input type="text" class="form-control" id="title" aria-describedby="emailHelp" name='title'/>
           </div>
           <div class="mb-3">
-            <label for="desc" class="form-label">Description</label>
+            <label htmlFor="desc" class="form-label">Description</label>
             <textarea class="form-control" id="desc" rows="3" name='desc'></textarea>
           </div>
           <button type="submit" class="btn btn-primary">Submit</button>
@@ -19,6 +22,12 @@ export default function Home() {
       </div>
       <div className="container my-3">
         <h2>Your Notes</h2>
+        {context.notes.map((note)=>{
+          return <div key={note.user}>
+            <h4>Title: {note.title}</h4>
+            <p>Description: {note.description}</p>
+          </div>
+        })}
       </div>
     </>
 
