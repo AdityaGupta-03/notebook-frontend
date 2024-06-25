f9b89b5650eef9d22a8de0b43a15084c0ef5cf97
Aditya Gupta <iaditya.gupta03@gmail.com>
2024-06-25 20:15:49 +0530

    Adding Modal

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -7,11 +7,6 @@ const NoteItem = (props) => {
         context.deleteNote(props.note._id);
     }
 
-    const onEdit = (e) => {
-        e.preventDefault();
-        context.editNote(props.note._id);
-    }
-
     return (
         <>
             <div className="card col-md-3 my-3 mx-3">
@@ -19,45 +14,11 @@ const NoteItem = (props) => {
                     <h5 className="card-title">{props.note.title}</h5>
                     <h6 className="card-subtitle mb-2 text-body-secondary">{props.note.tag}</h6>
                     <p className="card-text">{props.note.description}</p>
-                    <button type="button" className="btn btn-dark" data-toggle="modal" data-target="#exampleModalCenter"> <i className="fa-solid fa-pen-to-square"></i> Edit </button>
+                    <button type="button" className="btn btn-dark" onClick={() => { props.updateNote(props.note) }}> <i className="fa-solid fa-pen-to-square"></i> Edit </button>
                     <button type="button" className="btn btn-danger mx-3" onClick={onDelete}> <i className="fa-solid fa-trash"></i> Delete </button>
                 </div>
             </div>
-
-            <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLongTitle">Edit this Note</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            <form>
-                                <div class="form-group">
-                                    <label for="title" class="col-form-label">Title:</label>
-                                    <input type="text" class="form-control" id="title" />
-                                </div>
-                                <div class="form-group">
-                                    <label for="tag" class="col-form-label">Tag:</label>
-                                    <input type="text" class="form-control" id="tag" />
-                                </div>
-                                <div class="form-group">
-                                    <label for="description" class="col-form-label">Description:</label>
-                                    <textarea class="form-control" id="description"></textarea>
-                                </div>
-                            </form>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary" onClick={onEdit}>Save changes</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </>
-
     )
 }
 
--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useRef } from 'react'
 import notesContext from '../context/Notes/NotesContext'
 import NoteItem from './NoteItem';
 
@@ -6,20 +6,57 @@ const Notes = () => {
     const context = useContext(notesContext);
 
     // Just like ComponentDidMount
-    useEffect(()=>{
+    useEffect(() => {
         context.getNotes();
         // eslint-disable-next-line
-    },[]);
+    }, []);
+
+    const ref = useRef(null);
+    const updateNote = (note) => {
+        ref.current.click();
+    }
 
     return (
         <div className="container my-3">
+            <button ref={ref} type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Launch demo modal
+            </button>
+            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h1 className="modal-title fs-5" id="exampleModalLabel">Edit Modal</h1>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <form>
+                                <div className="mb-3">
+                                    <label for="title" className="col-form-label">Title:</label>
+                                    <input type="text" className="form-control" id="title" />
+                                </div>
+                                <div className="mb-3">
+                                    <label for="tag" className="col-form-label">Tag:</label>
+                                    <input type="text" className="form-control" id="tag" />
+                                </div>
+                                <div className="mb-3">
+                                    <label for="desc" className="col-form-label">Description:</label>
+                                    <textarea className="form-control" id="desc"></textarea>
+                                </div>
+                            </form>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <h2>Your Notes</h2>
             <div className="row">
                 {context.notes.map((note) => {
-                    return <NoteItem note={note} key={note._id} />
+                    return <NoteItem note={note} updateNote={updateNote} key={note._id} />
                 })}
             </div>
-
         </div>
     )
 }
