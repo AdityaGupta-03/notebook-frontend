7a539662538f4c012b544ab324fa67318094a8eb
Aditya Gupta <iaditya.gupta03@gmail.com>
2024-06-26 19:59:29 +0530

    Adding Signup and Login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
 
 export default function Login() {
+    const [cred, setCred] = React.useState({
+        email:'',
+        password:''
+    });
+    const handleChange = (e)=>{
+        setCred({...cred,[e.target.name]:e.target.value});
+    }
+    const handleSubmit = async(e)=>{
+        e.preventDefault();
+        try {
+            const res = await fetch('http://localhost:5000/api/auth/login',{
+                method:'POST',
+                headers:{
+                    'Content-Type':'application/json'
+                },
+                body:JSON.stringify(cred)
+            });
+            const data = await res.json();
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
   return (
-    <div>
-      This is Login
-    </div>
+    <div className='container my-5'>
+            <h1>Login:</h1>
+            <form onSubmit={handleSubmit}>
+                <div className="my-3">
+                    <label htmlFor="email" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="email" aria-describedby="emailHelp" name='email' value={cred.email} onChange={handleChange}/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="password" name='password' value={cred.password} onChange={handleChange}/>
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        </div>
   )
 }
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,9 +1,76 @@
 import React from 'react'
 
 export default function Signup() {
-  return (
-    <div>
-      This is signup
-    </div>
-  )
+    const [name, setName] = React.useState({ fname: '', lname: '' });
+    const [cred, setCred] = React.useState({ email: '', password: '', cpassword: '' })
+
+    const handleChange = (e)=>{
+        const {id, value} = e.target;
+        if(id === 'fname'){
+            setName({...name, fname: value});
+        }
+        else if(id === 'lname'){
+            setName({...name, lname: value});
+        }
+        else if(id === 'email'){
+            setCred({...cred, email: value});
+        }
+        else if(id === 'password'){
+            setCred({...cred, password: value});
+        }
+        else if(id === 'cpassword'){
+            setCred({...cred, cpassword: value});
+        }
+    }
+    
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const host = "http://localhost:5000/api/auth";
+        // Backend API Call: To create User;
+        if(cred.password === cred.cpassword){
+            const url = `${host}/createUser`;
+            const response = await fetch(url, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ name, email: cred.email, password: cred.password })
+            });
+            const json = response.json();
+            console.log(json);
+        }
+        else{
+            console.log("Password mismatch");
+        }
+    }
+    return (
+        <div className='container my-5'>
+            <h1>Signup:</h1>
+            <form onSubmit={handleSubmit}>
+                <div className="row my-3">
+                    <div className="col">
+                        <label htmlFor="fname">First Name: </label>
+                        <input type="text" className="form-control" placeholder="First name" aria-label="First name" id='fname' value={name.fname} onChange={handleChange}/>
+                    </div>
+                    <div className="col">
+                        <label htmlFor="lname"> Last Name: </label>
+                        <input type="text" className="form-control" placeholder="Last name" aria-label="Last name" id='lname' value={name.lname} onChange={handleChange}/>
+                    </div>
+                </div>
+                <div className="my-3">
+                    <label htmlFor="email" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="email" aria-describedby="emailHelp" name='email' value={cred.email} onChange={handleChange}/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="password" name='password' value={cred.password} onChange={handleChange}/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="c_password" className="form-label"> Confirm Password</label>
+                    <input type="password" className="form-control" id="c_password" name='c_password' value={cred.cpassword} onChange={handleChange}/>
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    )
 }
